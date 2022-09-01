54a29bb04a0195dd02ce6b3096a8b87826379af4
Avay kumar shaw <111166830+avay0809@users.noreply.github.com>
2022-09-01 22:28:24 +0530

    Add files via upload

---
+++ assignment 1/ques11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int hour,minute;
+    printf("Enter Time\n");
+    scanf("%d:%d",&hour,&minute);
+
+    printf("%d hour and %d Minute",hour,minute);
+    return 0;
+}
---
+++ assignment 1/ques4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    float a,r;
+    printf("Enter the radius\n");
+    scanf("%f", &r);
+
+    a = 3.14*r*r;
+    printf("Area of circle is %f having the radius %f",a,r);
+    return 0;
+}
